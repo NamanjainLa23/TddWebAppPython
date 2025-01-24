c7a9a0988278302e24da4eefce1146b34d56c066
namanjn23 <naman.jain@xperi.com>
2025-01-24 10:07:42 +0530

    Functional test now checks if we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,22 +14,36 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        #methods that start with test_ are recognized as test cases by the unittest framework
         # User has heard of a cool new online to-do app.
         #He goes to checkout it's homepage
+
         self.browser.get('http://localhost:8000')
 
+        #verifies if the string to-do in in title, if not it fails the test
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #he is invited to enter a to-do item straightaway
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.getattribute('placeholder'), 'Enter a ro-do item')
 
         #he types "Buy Medicines" into a text box
+        inputbox.send_keys('Buy Medicines')
 
         #when he presses enter, the page updates, and now the page lists
         #1: Buy Medicines" as an item in to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy Medcines' for row in rows))
 
 	#there is still a textbox inviting him to add another item. He
 	#enter "this medicine is for me"
+        self.fail('Finish the test!')
 
 	#the page updates again, and now shows both the items on his list
 
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,7 @@
+#django.tst.TestCase - subclass of python's unittest.Testcase
+#sets up test database automatically
 from django.test import TestCase
+
 from django.urls import resolve
 from django.http import HttpRequest
 
@@ -7,13 +10,24 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
+        """
+        uses resolve to map the root URL('/') ti outs corresponding view
+        Asserts that the function associated with '/' is home page.
+        Ensures that the root URL is correctly routed to the home page view
+        """
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+        #creates an httprequest to simulate a request to the home page view
         request = HttpRequest()
+
+        #calls the home page request to get the response
         response = home_page(request)
+
         html = response.content.decode('utf8')
+
+        #validates the home_page view return the correct html structure
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
